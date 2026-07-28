363fc3f8b4dd3744f7f3a39bb9b57ae228f8d71c
Yinan229 <645084218cyn@gmail.com>
2026-07-29 05:09:26 +0900

    Create __init__.py

---
+++ src/crews/simulation/__init__.py
@@ -0,0 +1,29 @@
+from .dropout import (
+    bernoulli_drop_rids,
+    make_S1,
+    make_S2,
+    make_S3,
+    make_S3_1,
+    make_S3_2,
+    make_S3_3,
+    make_S4,
+    make_S5,
+    SETTINGS,
+    TestMaskPlanner,
+)
+from .rx_cache import RxClassCache
+
+__all__ = [
+    "bernoulli_drop_rids",
+    "make_S1",
+    "make_S2",
+    "make_S3",
+    "make_S3_1",
+    "make_S3_2",
+    "make_S3_3",
+    "make_S4",
+    "make_S5",
+    "SETTINGS",
+    "TestMaskPlanner",
+    "RxClassCache",
+]
